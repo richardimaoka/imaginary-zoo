84bc13c11c1f62348db9240c80b420fa6e20c613
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-10-05 01:58:26 +0900

    manul cat

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import manul1 from "/public/images/manul-2162660_1280.jpg";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>マヌルネコ</h1>
+      <Image src={manul1} alt="manul cat" />
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
         essentially unchanged. It was popularised in the 1960s with the release
@@ -14,7 +18,13 @@ export default function Home() {
         with desktop publishing software like Aldus PageMaker including versions
         of Lorem Ipsum.
       </p>
-      <div></div>
+      <p>
+        It has survived not only five centuries, but also the leap into
+        electronic typesetting, remaining essentially unchanged. It was
+        popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software
+        like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
     </div>
   );
 }
