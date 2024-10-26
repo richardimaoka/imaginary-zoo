91534514c8784d390bd7e127770a8781ef09c0d1
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-10-26 20:16:59 +0900

    step 2: current link highlight

--- nextjs/app/components/sidebar/LinkInternal.tsx
+++ nextjs/app/components/sidebar/LinkInternal.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import styles from "./LinkInternal.module.css";
 
@@ -7,8 +10,14 @@ interface Props {
 }
 
 export function LinkInternal(props: Props) {
+  const pathname = usePathname();
+  const current = pathname === props.path;
+
   return (
-    <Link className={styles.component} href={props.path}>
+    <Link
+      className={styles.component + (current ? " " + styles.current : "")}
+      href={props.path}
+    >
       {props.name}
     </Link>
   );
